a24a571c9e2ee6bf3f63b72151ce3ab32a2c76f3
Anvidha <anvidhawork@gmail.com>
2026-09-09 17:31:14 +0530

    commit

--- l6-09-09/l6_7.c
+++ l6-09-09/l6_7.c
@@ -6,30 +6,30 @@
 
 int main()
 {
-    int n;
+    int n; 
     char *str;
     char x, y;
-
+// taking input of length
     printf("Enter length of word: ");
     scanf("%d", &n);
 
     str = (char *)malloc((n + 1) * sizeof(char));
-
+//taking input of word
     printf("Enter word: ");
     scanf("%s", str);
-
+//the letter to be replaced 
     printf("Enter x: ");
     scanf(" %c", &x);
-
+// the letter to replace
     printf("Enter y: ");
     scanf(" %c", &y);
-
+//loop to replace
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == x)
             str[i] = y;
     }
-
+// printing the new word
     printf("Modified word: %s\n", str);
 
     free(str);
